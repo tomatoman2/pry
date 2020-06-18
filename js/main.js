23a0e942632ca1dc657158eb34adbf18e5066c27
tomatoman2 <kasuga01m@gmail.com>
2020-06-18 23:02:40 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+  arrows: false,
+  autoplay: true,
+  /* ポイントここから～ */
+  autoplaySpeed: ,
+  cssEase: 'linear',
+  // 無限スクロールにするかどうか。最後の画像の次は最初の画像が表示される。
+  infinite: true,
+  // slidesToShow: 3,
+  // slidesToScroll: 3,
+});
