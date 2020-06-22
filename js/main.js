6e50d459184f3de770c31f2192318598e71429ac
tomatoman2 <kasuga01m@gmail.com>
2020-06-22 23:27:08 +0900

    Add　slickカルーセル

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 $('.slider').slick({
-  arrows: false,
-  autoplay: true,
-  /* ポイントここから～ */
-  autoplaySpeed: ,
-  cssEase: 'linear',
-  // 無限スクロールにするかどうか。最後の画像の次は最初の画像が表示される。
-  infinite: true,
-  // slidesToShow: 3,
-  // slidesToScroll: 3,
+  autoplay: true, //自動再生
+  // infinite: true, //スライドのループ有効化
+  dots: true, //ドットのナビゲーションを表示
+  slidesToShow: 5, //表示するスライドの数
+  slidesToScroll: 1, //スクロールで切り替わるスライドの数
+  responsive: [{
+    breakpoint: 768, //ブレークポイントが768px
+    settings: {
+      slidesToShow: 5, //表示するスライドの数
+      slidesToScroll: 1, //スクロールで切り替わるスライドの数
+    }
+  }, {
+    breakpoint: 480, //ブレークポイントが480px
+    settings: {
+      slidesToShow: 5, //表示するスライドの数
+      slidesToScroll: 1, //スクロールで切り替わるスライドの数
+    }
+  }]
 });
+
